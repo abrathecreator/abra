05060d301ff91360655399ef93c5596ba82bdc2c
ABRA CADABRA <abracadabra@MacBook-Air-ABRA.local>
2026-08-21 00:00:19 +0300

    fix: base path for cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/abra/" : "/",
+  base: "/",
   build: {
     sourcemap: false,
     minify: "esbuild",

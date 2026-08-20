3ebba4fc81ed61edafc3d6d6775564597247b081
ABRA CADABRA <abracadabra@MacBook-Air-ABRA.local>
2026-08-20 23:57:09 +0300

    add security headers

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/abra/" : "/",
+  build: {
+    sourcemap: false,
+    minify: "esbuild",
+  },
 }));
